9dbf624891ceee87f68a49bf8c232ce99215be53
Javad Ghanbari <javadghanbari.put@gmail.com>
2023-08-16 11:45:23 -0700

    adding fonts to app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        vazir : ["vazir"]
+      }
+    },
   },
   plugins: [],
 }
